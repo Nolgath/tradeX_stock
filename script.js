b9318206d0ff183e4f2d3c481375d83ccaeadebb
Cristian <your_email@example.com>
2025-12-10 14:27:33 +0000

    brands and model selections

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
-// ---------- TOGGLE SUB MENU LISTS ----------
-let btns = document.querySelectorAll("button")
-btns.forEach(btn =>{
-    btn.addEventListener("click", toggleList)
-})
+document.addEventListener("DOMContentLoaded", () => {
+    let checkboxes = document.querySelectorAll("#brands_available input[type='checkbox']");
 
-function toggleList(e){
-    e.target.parentNode.nextElementSibling.classList.toggle("hidden")
+    for(let checkbox of checkboxes){
+        checkbox.addEventListener('click', function () {
+            let selected_brands_arr = [...checkboxes].filter(checkbox => checkbox.checked == true).map(checkbox => checkbox.value.toLowerCase())
+            document.querySelector('#models_available').innerHTML = ""
+            displayModelsByBrand(selected_brands_arr)
+        })  
+    }
+});
+
+let btns = document.querySelectorAll("button")
+
+// ---------- RUN WHEN WEBSITE IS FULLY LOADED ----------
+let {the_cars} = JSON.parse(localStorage.getItem('car'))
+
+
+// ---------- GET UNIQUE BRANDS LOWERCASED FROM Hersteller.  ----------
+// --------BRANDS
+const all_brands = the_cars.map(car => car.Hersteller.toLowerCase())
+
+// ---------- MODELS SECTION  ----------
+const all_models = the_cars.map(car => String(car.Modell).toLowerCase())
+
+
+// ---------- TOGGLE SUB MENU LISTS ----------
+
+function toggleBtns(){
+    btns.forEach(btn =>{
+        btn.addEventListener("click", function toggleList(e){
+            e.target.parentNode.nextElementSibling.classList.toggle("hidden")
+        })
+    })
 }
 
+function init(){
+    toggleBtns()
+}
+
+init()
+
 // ---------- RETRIEVE LOCAL STORAGE DATA ----------
-let {the_cars} = JSON.parse(localStorage.getItem('car'))
 //displayCarsTable(the_cars)
 
 function displayCarsTable(array){
@@ -27,57 +58,37 @@ function displayCarsTable(array){
     });
 }
 
-/*
-// ---------- FILTER BY VIN  ----------
-let input_vin = document.querySelector('#vin_search_bar')
-
-input_vin.addEventListener('change', () => {
-    let vin = input_vin.value
-    let tbody = document.querySelector('tbody')
-
-    if(vin.length  == 0){
-        displayCarsTable(the_cars)
-        return
-    }
-
-    if(vin.length !== 17){
-        alert('Please input 17 characters')
-        return
-    }
-
-    tbody.innerHTML = ''
-    let filtered_cars = the_cars.filter(c => c.FIN == vin)
-    displayCarsTable(filtered_cars)
-})
-*/
-
-
-// ---------- GET UNIQUE BRANDS LOWERCASED FROM Hersteller.  ----------
-//Select a field from the array's objects. 
-// --------BRANDS
-const all_brands = the_cars.map(car => car.Hersteller.toLowerCase())
-// --------BRANDS
-const all_models = the_cars.map(car => String(car.Modell).toLowerCase())
-
+// ---------- FILTERS BY VIN  ----------
+function filter_by_vin(vin){
+    let input_vin = document.querySelector('#vin_search_bar')
+    input_vin.addEventListener('change', () => {
+        let vin = input_vin.value
+        let tbody = document.querySelector('tbody')
+    
+        if(vin.length  == 0){
+            displayCarsTable(the_cars)
+            return
+        }
+    
+        if(vin.length !== 17){
+            alert('Please input 17 characters')
+            return
+        }
+    
+        tbody.innerHTML = ''
+        let filtered_cars = the_cars.filter(c => c.FIN == vin)
+        displayCarsTable(filtered_cars)
+    })
+}
 
 // ---------- COUNT PER BRAND ----------
-const count_brands = {}
-all_brands.forEach(brand => {
-    if(!Object.keys(count_brands).includes(brand)){
-        count_brands[brand]  = 1
-    }else{
-        count_brands[brand] = count_brands[brand] + 1
-    }
-})
 
-// ---------- COUNT PER MODEL ----------
-const count_models = {}
-all_models.forEach(model => {
-    if(!Object.keys(count_models).includes(model)){
-        count_models[model]  = 1
-    }else{
-        count_models[model] = count_models[model] + 1
+const count_brands = {}
+the_cars.forEach(car => {
+    if(!Object.keys(count_brands).includes(car.Hersteller)){
+        count_brands[car.Hersteller]  = []
     }
+    count_brands[car.Hersteller].push(car.Modell)
 })
 
 // ---------- ADD BRANDS TO BRANDS MENU ----------
@@ -86,18 +97,44 @@ function brandsAvailable() {
     let brands_available_li = document.querySelector('#brands_available')
 
     for (const [key,value] of Object.entries(count_brands)) {
-        brands_available_li.innerHTML += `<p class='selection'><input type="checkbox" class='checkbox'> ${key} (${value})</p>`
+        brands_available_li.innerHTML += 
+                                        `<p class='selection'>
+                                            <input type="checkbox" class='checkbox' value='${key}'> ${key} (${value.length})
+                                        </p>`
     }
 }
 // ---------- ADD MODELS TO MODELS MENU ----------
-modelsAvailable()
-function modelsAvailable() {
+// displayModels()
+function displayModelsByBrand(brands) {
     let models_available_li = document.querySelector('#models_available')
 
-    for (const [key,value] of Object.entries(count_models)) {
-        models_available_li.innerHTML += `<p class='selection'><input type="checkbox" class='checkbox'> ${key} (${value})</p>`
+    for(let [k, v] of Object.entries(count_brands)){
+        if(brands.includes(k.toLowerCase())){
+            let models = [...new Set([...v])]
+            models.forEach(model => {
+                models_available_li.innerHTML += `<p class='selection'><input type="checkbox" class='checkbox'> ${model} (${v.filter(m => m == model).length})</p>`
+
+            })
+        }
     }
+    
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+//brands -> Array of brands we selected
+//console.log(count_models);
+//REVIEW Set()
 
 
 /*
